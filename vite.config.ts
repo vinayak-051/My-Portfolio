9e144b04fcf7f49ac6cac6900f839362f5057323
vinayak-051 <vinayakramavath1@gmail.com>
2026-07-02 15:09:39 +0530

    Remove lovable-tagger, fix deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger"
-
-
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig((_) => ({
   base: "/My-Portfolio/",
   build: {
     outDir: "dist",
@@ -19,8 +16,7 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  ],
 
   resolve: {
     alias: {
